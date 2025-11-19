0c52f34bb83b0683b9955b9da99b5f706540d0dd
[Prayusha Pokhrel] <sibwawc91@gmail.com>
2025-11-19 13:56:42 +0545

    added another way

--- main2.py
+++ main2.py
@@ -1,13 +1,19 @@
 def palcheck(word):
+
     reverse="".join(reversed(word))
-    #if you do word.reverse(), reversed 'word' will be stored in word
-    if(word==reverse):
-        return 1
-    else:
-        return 0
+    ans=1
+
+    for i in range(len(word)):
+        if(reverse[i]!=word[i]):
+            ans=0
+            break
+
+    return ans
     
+
 user=input("Enter a word to check if it's palindrome or not ")
 ans=palcheck(user)
+
 
 if(ans==1):
     print(f"yes, {user} is a palindrome ")
